483b92cc9b9b117daa0dd059a523226084ddc0a7
OlehVoitovych <oleg.voytovych04@gmail.com>
2023-07-26 22:16:00 +0300

    1

---
+++ src/Perso/perso.java
@@ -0,0 +1,12 @@
+package Perso;
+
+import java.util.ArrayList;
+
+public class perso {
+    public String name;
+    public int level;
+    //public ArrayList<Item> Inventory;
+
+    public int weapon;
+    
+}
